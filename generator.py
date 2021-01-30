00fd6d1f2ffb606827efde2f49c0edf695302087
jamiegharrison24 <69954587+jamiegharrison24@users.noreply.github.com>
2021-01-30 14:13:41 +1100

    iterative mustache

--- generator.py
+++ generator.py
@@ -5,30 +5,38 @@ import numpy as np
 import math
 import PIL
 from PIL import Image #for image manipulation
-import os # 
+import os 
 
 
 
 directory = ''
 
 # images aare opened, note they must be resized for mask to work properly
-background = Image.open("test.png")
-background.resize((512, 512))
+elmo = Image.open("elmo.jpg")
+elmo.resize((512, 512))
 
-mustache = Image.open("mustache.png")
-mustache.resize((512,512))
 
-glasses = Image.open("glasses.png")
-glasses.resize((512,512))
+for i in os.listdir('Mustache'):
+    if i.endswith('.jpg') or i.endswith('.png'):
+            mustache = Image.open(i)
+            mustache.resize((512,512))
+            elmoCopy = elmo.copy()
+            elmo.paste(mustache,(0,0),mustache)
+            elmo.show()
 
-hat = Image.open("hat.png")
-hat.resize((512,512))
+
+
+
+# glasses = Image.open("glasses.png")
+# glasses.resize((512,512))
+
+# hat = Image.open("hat.png")
+# hat.resize((512,512))
 
 
 # Doing manually to learn how library works - note will have to tweak second parameter in function to get locatino correct
 
-background.paste(mustache, (0,0), mustache)
-background.paste(glasses, (0,0), glasses)
-background.paste(hat, (0,0), hat)
+# background.paste(mustache, (0,0), mustache)
+# background.paste(glasses, (0,0), glasses)
+# background.paste(hat, (0,0), hat)
 
-background.show()
